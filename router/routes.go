61fd3fb41bca63178349dda554452629fb73f91f
André Martins <andremartins@vipcommerce.com.br>
2023-11-18 17:03:09 -0300

    update routes

--- router/routes.go
+++ router/routes.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/andremartinsds/flash-cards-api/handler"
+	hDec "github.com/andremartinsds/flash-cards-api/handler/dec"
 	hUser "github.com/andremartinsds/flash-cards-api/handler/user"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -21,6 +22,12 @@ func initilizeRoutes(router *gin.Engine) {
 		api.GET("users", hUser.ListUserHandler)
 		api.DELETE("user", hUser.DeleteUserHandler)
 		api.PUT("user", hUser.UpdateUserHandler)
+
+		api.POST("dec", hDec.CreateDecHandler)
+		api.GET("dec", hDec.ReadDecHandler)
+		api.GET("decs", hDec.ListDecHandler)
+		api.DELETE("dec", hDec.DeleteDecHandler)
+		api.PUT("dec", hDec.UpdateDecHandler)
 	}
 
 }
