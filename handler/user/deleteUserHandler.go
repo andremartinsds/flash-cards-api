da66bcf3913bbb488a35ef5331493e0b09940ba1
André Martins <andremartins@vipcommerce.com.br>
2023-11-19 20:47:25 -0300

    Fix: Users can't be removed if they have a dec.

--- handler/user/deleteUserHandler.go
+++ handler/user/deleteUserHandler.go
@@ -12,18 +12,23 @@ func DeleteUserHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 
 	if id == "" {
-		sendError(ctx, http.StatusBadRequest, "the id is required")
+		sendError(ctx, http.StatusBadRequest, "An ID is required.")
 		return
 	}
 
 	var user schemas.User
-	if err := handler.DB.First(&user, "id = ?", id).Error; err != nil {
-		sendError(ctx, http.StatusNotFound, "does user not found")
+	if err := handler.DB.Preload("Dec").First(&user, "id = ?", id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "The user was not found.")
+		return
+	}
+
+	if userHasADec(user) {
+		sendError(ctx, http.StatusBadRequest, "This user has a 'dec,' so you can't delete him/her.")
 		return
 	}
 
 	if err := handler.DB.Delete(&user).Error; err != nil {
-		sendError(ctx, http.StatusNotFound, "we have a problem to remove it, please try again")
+		sendError(ctx, http.StatusNotFound, "We encountered a problem while trying to remove it. Please try again.")
 		return
 	}
 
@@ -31,4 +36,8 @@ func DeleteUserHandler(ctx *gin.Context) {
 
 	sendSuccess(ctx, http.StatusOK, "deleted", response)
 
+}
+
+func userHasADec(user schemas.User) bool {
+	return len(user.Dec) > 0
 }
